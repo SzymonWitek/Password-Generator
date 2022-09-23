cb5a5cef07e02d9c92bf7db65e942947441914c9
SzymonWitek <szymonwitek99@wp.pl>
2022-09-23 21:36:03 +0200

    Restore strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
@@ -8,10 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <div className="wrapper">
-      <App />
-    </div>
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="wrapper">
+        <App />
+      </div>
+    </ThemeProvider>
+  </React.StrictMode>
 );
